cc3ba9ef3dd07197e346643c479591f9becbf02c
Mikhail <mixaa21@bk.ru>
2022-03-14 17:27:32 +0300

    Second commit

---
+++ #src/js/watch.js
@@ -0,0 +1,9 @@
+let watched = document.querySelector('div.sidebar__dropdown > ul > li:nth-child(1)');
+let body = document.querySelector('.body__content');
+
+// watched.onclick = function() {
+//     body.classList.add("numbers")
+//     if (body.classList[1] === "numbers"){
+//         body.innerHTML = ` @@include("..\\Главы\\Глава 3 Типы значения и переменные\\3.2 Числа\\_numbers.html")`
+//     }
+// }
